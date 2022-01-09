a076e8d325d7d0688f36b1b2344894d996fd19b2
Adriana Calvo-Matos <adriana.calvomatos@gmail.com>
2022-01-08 21:37:39 -0500

    create pokedex problem

---
+++ firstBadVersion.js
@@ -0,0 +1,8 @@
+// 278. First Bad Version
+const solution = (isBadVersion) => {
+  /**
+   * @param {integer} n Total versions
+   * @return {integer} The first bad version
+   */
+  return function (n) {};
+};
