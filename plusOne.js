c6afaa356f8686794e169092691e518dcca0a2e3
Adriana Calvo-Matos <adriana.calvomatos@gmail.com>
2022-01-28 14:59:31 -0500

    refactor

--- plusOne.js
+++ plusOne.js
@@ -16,3 +16,29 @@ const plusOne = (digits) => {
   digits.unshift(1);
   return digits;
 };
+
+// failed attempt
+// const addition = (digits) => {
+//   let lastIndex = digits.length - 1;
+
+//   if (digits[lastIndex] < 9) {
+//     digits[lastIndex]++;
+//     return digits;
+//   }
+
+//   if (lastIndex > 0) {
+//     digits[lastIndex] = 0;
+//     digits[lastIndex - 1]++;
+//   }
+
+//   for (let i = lastIndex - 1; i >= 0; i--) {
+//     if (i === 0 && digits[i] === 10) {
+//       digits[i] = 1;
+//       digits.push(0);
+//     } else if (digits[lastIndex - 1] === 10) {
+//       digits[i] = 0;
+//       digits[i - 1]++;
+//     }
+//   }
+//   return digits;
+// };
