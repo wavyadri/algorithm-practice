f5f82f7d9b22b97e61935e3b672e660c581a1a28
Adriana Calvo-Matos <adriana.calvomatos@gmail.com>
2022-01-28 13:47:41 -0500

    refactor

--- maxSubArray.js
+++ maxSubArray.js
@@ -1,18 +1,21 @@
 // 53. Maximum Subarray
 // https://leetcode.com/problems/maximum-subarray/
+// divide and conquer
 
 const maxSubArray = (nums) => {
+  let sum = nums[0];
   let max = nums[0];
-  let current = Math.max(max, 0);
-
   for (let i = 1; i < nums.length; i++) {
-    current += nums[i]; // this checks if the next pair able to beat the running max?
-    max = Math.max(max, current);
-    current = Math.max(current, 0);
-    console.log(max, current);
+    if (nums[i] > sum + nums[i]) {
+      sum = nums[i];
+    } else {
+      sum += nums[i];
+    }
+    max = Math.max(max, sum);
   }
 
   return max;
 };
 
 console.log(maxSubArray([2, 1, -3, 4, -1, 2, 1, -5, 4]));
+// output: 6
