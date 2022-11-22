680ff6bfdfb0f91b4fdfd743bdad3d4d96050897
Adriana Calvo-Matos <adriana.calvomatos@gmail.com>
2022-11-22 17:52:12 -0500

    simplify

--- sum1DArray.ts
+++ sum1DArray.ts
@@ -2,15 +2,11 @@
 // https://leetcode.com/problems/running-sum-of-1d-array
 
 function runningSum(nums: number[]): number[] {
-  let sumArray: number[] = [];
+  let sumArray: number[] = [nums[0]];
 
-  for (let i = 0; i < nums.length; i++) {
-    if (i === 0) {
-      sumArray.push(nums[i]);
-    } else {
+  for (let i = 1; i < nums.length; i++) {
       let nextNum = nums[i] + sumArray[i - 1];
       sumArray.push(nextNum);
-    }
   }
 
   return sumArray;
