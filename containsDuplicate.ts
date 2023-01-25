751382385ad290cf57a95462808352dfe84398ab
Adriana Calvo-Matos <adriana.calvomatos@gmail.com>
2023-01-25 20:19:11 +0100

    complete q 217

---
+++ containsDuplicate.ts
@@ -0,0 +1,15 @@
+// 217. Contains Duplicate
+// https://leetcode.com/problems/contains-duplicate
+
+// v1
+function containsDuplicate(nums: number[]): boolean {
+  let tally: number[] = [];
+  for (const num of nums) {
+    if (tally.includes(num)) {
+      return true;
+    }
+    tally.push(num);
+  }
+
+  return false;
+}
