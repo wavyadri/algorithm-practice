7513f49b0d83cb117b1307ee67add36c4a9eaa03
Adriana Calvo-Matos <adriana.calvomatos@gmail.com>
2023-03-09 13:12:27 +0100

    complete q 219 refactor

--- containsDuplicateII.ts
+++ containsDuplicateII.ts
@@ -1,5 +1,7 @@
 // 219. Contains Duplicate II
 // https://leetcode.com/problems/contains-duplicate-ii/
+// T: O(n)
+// S: O(n)
 
 function containsNearbyDuplicate(nums: number[], k: number): boolean {
   const obj = {};
@@ -15,6 +17,22 @@ function containsNearbyDuplicate(nums: number[], k: number): boolean {
         obj[nums[i]].push(i);
       }
     }
+  }
+
+  return false;
+}
+
+function containsNearbyDuplicateV2(nums: number[], k: number): boolean {
+  const map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(nums[i])) {
+      if (i - map.get(nums[i]) <= k) return true;
+    }
+
+    // instead of keeping an array of all values,
+    // always replace with curr value to keep track of latest i
+    map.set(nums[i], i);
   }
 
   return false;
